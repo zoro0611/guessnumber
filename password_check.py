a3d2376c3c3a6c129df6c366cad6c335d54e3090
Leo Hsu <zoro061181249@gmail.com>
2021-01-22 11:42:07 +0800

    1. 解決 剩餘0次時，不會再告知剩餘0次機會的訊息 2. 將剩餘機會的語法移動到while迴圈一開始位置(優化)

--- password_check.py
+++ password_check.py
@@ -5,13 +5,16 @@ left_chance = 3 #剩餘機會
 # 對的話， 就印出 "登入成功!"
 
 while left_chance > 0: #迴圈開始
+    left_chance = left_chance - 1
     user_type = input("請輸入密碼: ")
     if user_type == password:
         print("登入成功!")
         break #逃出迴圈
     else:
-        left_chance = left_chance - 1
-        print("密碼錯誤! 還有", left_chance ,"次機會")
-
+        print("密碼錯誤!")
+        if left_chance > 0:
+            print("還有", left_chance, "次機會")
+        else:
+            print("沒有機會嘗試了，要鎖帳號了啦!")
 #這邊停止迴圈
 
