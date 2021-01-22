3ceb50705b3e2d1c229f051cdd488c63c022ba59
Leo Hsu <zoro061181249@gmail.com>
2021-01-22 11:20:51 +0800

    Initial commit

---
+++ password_check.py
@@ -0,0 +1,16 @@
+password = 'a123456'
+left_chance = 3 #剩餘機會
+#讓使用者最多輸入3次密碼
+# 不對的話 印出"密碼錯誤! 還有_次機會"
+# 對的話， 就印出 "登入成功!"
+
+while True: #迴圈開始
+    user_type = input("請輸入密碼: ")
+    if user_type == password:
+        print("登入成功!")
+        break #逃出迴圈
+    else:
+        left_chance = left_chance - 1
+        print("密碼錯誤! 還有", left_chance ,"次機會")
+        if left_chance == 0:
+            break
