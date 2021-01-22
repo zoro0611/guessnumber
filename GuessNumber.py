0192ced58e9d5147eb38fe476a2ba0e27da86d9b
Leo Hsu <zoro061181249@gmail.com>
2021-01-22 17:21:16 +0800

    增加讓使用者自行選擇隨機變數

--- GuessNumber.py
+++ GuessNumber.py
@@ -1,10 +1,12 @@
 import random
+start = int(input("請決定隨機數字開始值:"))
+end = int(input("請決定隨機數字結束值: "))
 
-r = random.randint(1, 100) # 使用【random】裡面的【隨機整數】，定義隨機整數範圍落在1~100裏頭
+r = random.randint(start, end) # 使用【random】裡面的【隨機整數】，定義隨機整數範圍落在1~100裏頭
 count = 0
 while True:
     count += 1   # count = count + 1
-    guess = int(input("請從1-100裡猜一個數字: "))
+    guess = int(input("請猜一個數字: "))
     if guess > r:
         print("你猜的太大了")
     elif guess < r:
